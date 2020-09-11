032ea958549ded1f7d32b14b7bdf679372a99e07
Kosteyyy <averkiev@yandex.ru>
2020-09-11 18:25:12 +0300

    GraphQL server Up to create Issue on server mutation

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,9 @@
-const initialIssues = [
-    {
-        id: 1, status: 'New', owner: 'Ravan', effort: 5,
-        created: new Date('2018-08-05'), due: undefined, 
-        title: 'Error in console when clicking Add',
-    },
-    {
-        id: 2, status: 'Assigned', owner: 'Eddie', effort: 14,
-        created: new Date('2018-08-16'), due: new Date('2018-08-30'),
-        title: 'Missing bottom border on panel',
-    }
-];
+const dateRegex = new RegExp('^\\d\\d\\d\\d-\\d\\d-\\d\\d');
+
+function jsonDateReviver(key, value) {
+    if (dateRegex.test(value)) return new Date(value);
+    return value;
+}
 
 class IssueFilter extends React.Component {
     render() {
@@ -52,7 +46,7 @@ function IssueRow (props) {
             <td>{issue.owner}</td>
             <td>{issue.created.toDateString()}</td>
             <td>{issue.effort}</td>
-            <td>{issue.due ? issue.due.toDateString() : ''}</td>
+            <td>{issue.due ? issue.due.toDateString() : ' '}</td>
             <td>{issue.title}</td>
         </tr>
     );
@@ -69,7 +63,8 @@ class IssueAdd extends React.Component {
         e.preventDefault();
         const form = document.forms.issueAdd;
         const issue = {
-            owner: form.owner.value, title: form.title.value, status: 'New',
+            owner: form.owner.value, title: form.title.value,
+            due: new Date(new Date().getTime() + 1000*60*60*24*10),
         }
         this.props.createIssue(issue);
         form.owner.value = "";
@@ -79,8 +74,8 @@ class IssueAdd extends React.Component {
     render() {
         return (
             <form name="issueAdd" onSubmit={this.handleSubmit}>
-                <input type="text" name="owner" placeholder="Owner" />
-                <input type="text" name="title" placeholder="Title" />
+                <input type="text" name="owner" placeholder="Owner" required />
+                <input type="text" name="title" placeholder="Title" required/>
                 <button>Add</button>
             </form>
         );
@@ -99,10 +94,24 @@ class IssueList extends React.Component {
         this.loadData();
     }
 
-    loadData() {
-        setTimeout(() => {
-            this.setState({ issues: initialIssues });
-        }, 500);
+    async loadData() {
+        const query = `query {
+            issueList {
+              id title status owner
+              created effort due
+            }
+          }`;
+
+        const response = await fetch('/graphql', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json'},
+            body: JSON.stringify({ query })
+        });
+
+        const body = await response.text();
+
+        const result = JSON.parse(body, jsonDateReviver);
+        this.setState({ issues: result.data.issueList });
     }
     
     createIssue(issue) {
@@ -118,7 +127,7 @@ class IssueList extends React.Component {
     render() {
         return (
             <React.Fragment>
-                <h1>Issue Tracker func</h1>
+                <h1>Issue Tracker ServerResponse</h1>
                 <IssueFilter />
                 <hr />
                 <IssueTable issues={this.state.issues} />
